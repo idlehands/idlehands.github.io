20c8c5c2bdfb70dd0a607164c8a332bd2091ec45
Karl Matthias <karl@matthias.org>
2013-08-22 17:07:09 +0100

    Detect mime types for files to upload. Use them when uploading.

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'jekyll-minibundle'
 gem 'coderay'
 gem 'rake'
 gem 'aws-sdk'
+gem 'mime-types'
 
 group :development do
   gem 'rspec'
--- lib/s3_client.rb
+++ lib/s3_client.rb
@@ -1,4 +1,5 @@
 require 'aws-sdk'
+require 'mime-types'
 
 class S3Client
   def initialize(access_key, secret_access_key, bucket)
@@ -7,19 +8,21 @@ class S3Client
       secret_access_key: secret_access_key,
     )
   
-  p bucket
     @s3 = AWS::S3.new(s3_endpoint: 's3-us-west-2.amazonaws.com')
     @bucket = @s3.buckets[bucket]
   end
 
   def upload(file_list)
+    puts "Bucket: #{@bucket}"
     file_list.inject([]) do |memo, file|
       unless Dir.exists?(file)
         print "uploading #{file}..."
 
-        s3_filename = file.sub(/^.*?\//, '')
-        memo << s3_filename
-        (@bucket.objects[s3_filename] || @bucket.object.create(s3_filename)).write(Pathname.new(file))
+        strip_leading_slashes(file).tap do |s3_filename|
+          memo << s3_filename
+          mime_type = mime_type_for(file)
+          get_or_create_object(s3_filename).write(Pathname.new(file), content_type: mime_type)
+        end
 
         puts 'done'
       end
@@ -35,7 +38,25 @@ class S3Client
     end
   end
 
+  private 
+
   def s3_filelist
     @bucket.objects.map { |x| x.key }
+  end
+
+  def get_or_create_object(s3_filename)
+    @bucket.objects[s3_filename] || @bucket.object.create(s3_filename)
+  end
+
+  def strip_leading_slashes(filename)
+    filename.sub(/^.*?\//, '')
+  end
+
+  def mime_type_for(file)
+    case (type = MIME::Types.type_for(file).first)
+      when nil then 'text/html'
+      when 'text/html' then 'text/html; charset=utf-8'
+      else type
+    end
   end
 end
